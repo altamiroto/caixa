1d6ea5860174cf748dd7388f2474c0ba7aaf0992
altamiroto <altamirojms@gmail.com>
2025-08-12 11:39:27 -0300

    Update produtos.js

--- api/produtos.js
+++ api/produtos.js
@@ -1,17 +1,6 @@
 export default async function handler(req, res) {
-  try {
-    const response = await fetch("https://api.beteltecnologia.com/produtos", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "access-token": process.env.ACCESS_TOKEN,
-        "secret-access-token": process.env.SECRET_TOKEN
-      }
-    });
-
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ error: "Erro ao buscar produtos" });
-  }
+  res.status(200).json({
+    ACCESS_TOKEN: process.env.ACCESS_TOKEN,
+    SECRET_TOKEN: process.env.SECRET_TOKEN
+  });
 }
