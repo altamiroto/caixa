74498710da4e2865d01d514572b805b223170a65
altamiroto <altamirojms@gmail.com>
2025-08-12 12:08:36 -0300

    Create lojas.js

---
+++ gestaoclick/api/lojas.js
@@ -0,0 +1,22 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch("https://api.beteltecnologia.com/lojas", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "access-token": process.env.ACCESS_TOKEN,
+        "secret-access-token": process.env.SECRET_TOKEN,
+      },
+    });
+
+    if (!response.ok) {
+      const erro = await response.json();
+      return res.status(response.status).json(erro);
+    }
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao buscar lojas" });
+  }
+}
